0a0e9694a13bf87006893dbad7cb54c017856049
Lisa Ann Yu <lisaann.yu@gmail.com>
2018-09-25 21:52:40 -0700

    move everything to .Rmd and reorganize

--- predict_MR.R
+++ predict_MR.R
@@ -56,6 +56,32 @@ table_preds <- table_10 %>%
          -scorecard_median_earnings_2011)
 summary(lm(mr_kq5_pq1 ~ .,
            data = table_preds))
+# exp_instr_pc_2013, hbcu, ipeds_enrollment_2013, ipeds_enrollment_2000, endowment_pc_2000,
+#   asian_or_pacific_share_fall_2000, black_share_fall_2000, hisp_share_fall_2000
+#   pct_arthuman_2000, pct_business_2000, pct_health_2000, pct_publicsocial_2000,
+#   pct_stem_2000, pct_socialscience_2000, pct_tradepersonal_2000, 
+#   adjmortmeas_chfall30day, cs00_seg_inc_pov25, poor_share, frac_middleclass,
+#   scap_ski90pcm, cs_frac_black, cs_frac_hisp, cs_labforce, pop_density, 
+#   frac_traveltime_lt15, hhinc00, taxrate
+
+# Check missingness
+vars_w_missingness <- table_preds %>% 
+  purrr::map_int(~ sum(is.na(.))) %>% 
+  .[. != 0] %>% 
+  names()
+
+## Add column to denote missingness
+add_missing_col <- function(df, var) {
+  # Add 1/0 col, change NA to -1 in original column
+  df[paste0(var, "_NA")] <- if_else(is.na(df[var]), 1, 0)
+  
+  df[var] <- if_else(is.na(df[var]), -1, df[var]) # don't think this will work
+}
+
+add_missing_col(table_preds, "female")
+
+table_preds %>% 
+  mutate(exp_instr_pc_2000_NA = if_else(is.na(exp_instr_pc_2000), 1, 0))
 
 # Elastic net: probably correlated factors
 library(glmnet)
